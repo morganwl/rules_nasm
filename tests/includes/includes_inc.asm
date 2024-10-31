bf830ea90f4655c772e28b92b04a275b2284f1fa
Morgan Wajda-Levie <morgan.wajdalevie@gmail.com>
2024-10-31 13:18:53 -0400

    removed symlink

--- tests/includes/includes_inc.asm
+++ tests/includes/includes_inc.asm
@@ -1,1 +1,8 @@
-/home/morgan/code/rules_nasm/tests/includes_inc.asm
+; Copyright (c) 2024 Morgan Wajda-Levie.
+; 
+; Define an addition macro.
+%macro MYAD 3
+    mov %1, %2
+    add %1, %3
+%endmacro
+
