ba5651fc8110358db833b964fedefbf3dffb3bbd
UebelAndre <github@uebelandre.com>
2025-05-15 13:33:39 +0000

    Deploying to gh-pages from @ morganwl/rules_nasm@de19fe0ebfe4f7185fe2d6d916159d057de50f1e 🚀

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="index.html">Introduction</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="index.html">Introduction</a></li><li class="chapter-item expanded affix "><a href="rules.html">Rules</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
